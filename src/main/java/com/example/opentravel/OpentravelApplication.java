91c57f9194798f7e9605dfedbf1688a7c12fcac7
tanya <tansuluu.myrzaeva@iaau.edu.kg>
2019-02-27 22:21:27 +0600

    Initial commit

---
+++ src/main/java/com/example/opentravel/OpentravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.opentravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentravelApplication.class, args);
+	}
+
+}
