c0efd1d77820ad63001179273439e098eca7ffde
tanya <tansuluu.myrzaeva@iaau.edu.kg>
2019-02-28 23:56:50 +0600

    Login controller added

---
+++ src/main/java/com/example/opentravel/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.example.opentravel.controller;
+
+import com.example.opentravel.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.transaction.Transactional;
+
+@Controller
+@Transactional
+public class LoginController {
+    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+}
