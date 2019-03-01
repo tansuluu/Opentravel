b56c19e0ea704cccf9ef90b59931397a7b1f8113
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2019-03-01 20:12:24 +0600

    Blog model deleted

--- src/main/java/com/example/opentravel/repository/PlaceRepository.java
+++ src/main/java/com/example/opentravel/repository/PlaceRepository.java
@@ -1,6 +1,5 @@
 package com.example.opentravel.repository;
 
-import com.example.opentravel.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
