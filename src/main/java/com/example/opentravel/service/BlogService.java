b806d9c2ddc4fd3083310794e49f6bc122c50134
nadyrbek97 <nadyrbek97@gmail.com>
2019-03-04 21:00:33 +0600

    Blog Repository and Blog Service added

---
+++ src/main/java/com/example/opentravel/service/BlogService.java
@@ -0,0 +1,31 @@
+package com.example.opentravel.service;
+import com.example.opentravel.model.Blog;
+import com.example.opentravel.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
+@Service("BlogService")
+public class BlogService {
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    public Blog findById(long id){
+        return blogRepository.findById(id);
+    }
+
+    public Blog updateView(long id,int n){
+        Blog blog=findById(id);
+        blog.setView(blog.getView()+n);
+        return save(blog);
+    }
+
+    public Blog save(Blog blog){
+        blog.setDate(Calendar.getInstance().getTime());
+        blog.setSmallText(blog.getText().substring(0,100)+"...");
+        return blogRepository.save(blog);
+    }
+
+}
