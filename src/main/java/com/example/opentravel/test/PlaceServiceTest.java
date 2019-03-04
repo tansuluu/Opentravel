2c9d5366227d6e69e4666caf076558954d0785dd
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2019-03-04 21:07:55 +0600

    unit testing wrote and passed for User service function Find by name!

--- src/main/java/com/example/opentravel/test/PlaceServiceTest.java
+++ src/main/java/com/example/opentravel/test/PlaceServiceTest.java
@@ -4,16 +4,18 @@ package com.example.opentravel.test;
 import com.example.opentravel.model.Place;
 import com.example.opentravel.repository.PlaceRepository;
 import com.example.opentravel.service.PlaceService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 
 import static org.mockito.Mockito.when;
 
@@ -26,12 +28,37 @@ public class PlaceServiceTest {
     @InjectMocks
     PlaceService placeService;
 
+    @Before
+    public void setUp(){
+        System.out.println("Starting PlaceServiceTest class!");
+    }
+
+    @After
+    public void terminate(){
+        System.out.println("PlaceServiceTest class is terminated");
+    }
     @Test
     public void testFindById(){
         Place place=new Place();
+        long id=1;
         place.setId(1);
-        when(placeRepositoryMock.findById(1)).thenReturn(place);
-        assertEquals(place,placeService.findById(1));
+        when(placeRepositoryMock.findById(id)).thenReturn(place);
+        assertEquals(place,placeService.findById(id));
+    }
+
+    @Test
+    public void findByUsarname(){
+        ArrayList<Place> list=new ArrayList<>();
+        Place place=new Place();
+        String name="Islam";
+        place.setUsarname("Islam");
+        Place place1=new Place();
+        place1.setUsarname("Islam");
+        list.add(place);
+        list.add(place1);
+        when(placeRepositoryMock.findByUsarname(name)).thenReturn(list);
+        assertEquals(list,placeService.findByUsarname(name));
+
     }
 
 
