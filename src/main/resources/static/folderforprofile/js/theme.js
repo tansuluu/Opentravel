c88d678d6bc75c0175c7d7a603bcccee358a1c6b
ElmiraKurmanbekova <43088578+ElmiraKurmanbekova@users.noreply.github.com>
2019-03-07 12:23:06 +0600

    Theme.js was updated

--- src/main/resources/static/folderforprofile/js/theme.js
+++ src/main/resources/static/folderforprofile/js/theme.js
@@ -93,6 +93,64 @@
     blog_slider();
     
     
- 
+  /*----------------------------------------------------*/
+    /*  Google map js
+    /*----------------------------------------------------*/
+    
+    if ( $('#mapBox').length ){
+        var $lat = $('#mapBox').data('lat');
+        var $lon = $('#mapBox').data('lon');
+        var $zoom = $('#mapBox').data('zoom');
+        var map = new GMaps({
+            el: '#mapBox',
+            lat: $lat,
+            lng: $lon,
+            scrollwheel: false,
+            scaleControl: true,
+            streetViewControl: false,
+            panControl: true,
+            disableDoubleClickZoom: true,
+            mapTypeControl: false,
+            zoom: $zoom,
+                styles: [{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"}]}]
+            });
+        
+        }
+    
+        
+//    
+//        $('.header_area .nav.navbar-nav li').click(function(e) {
+//            e.preventDefault(); //prevent the link from being followed
+//            $('.header_area .nav.navbar-nav li').removeClass('active');
+//            $(this).addClass('active');
+//        });
+    
+    
+    $('.header_area .nav.navbar-nav li a[href^="#"]:not([href="#"])').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 80
+        }, 1500);
+        event.preventDefault();
+    });
+    
+    
+    function bodyScrollAnimation() {
+        var scrollAnimate = $('body').data('scroll-animation');
+        if (scrollAnimate === true) {
+            new WOW({
+                mobile: false
+            }).init()
+        }
+    }
+    bodyScrollAnimation();
+    
+    
+    // preloader js
+    $(window).load(function() { // makes sure the whole site is loaded
+		$('#preloader_spinner').fadeOut(); // will first fade out the loading animation
+		$('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+		$('body').delay(150).css({'overflow':'visible'})
+})
     
 })(jQuery)
