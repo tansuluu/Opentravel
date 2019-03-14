8c6fc214829a558e1fdc35f8f3696029bf451eb6
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2019-03-14 22:40:06 +0600

    Login test added

--- src/main/java/com/example/opentravel/test/BlogControllerTest.java
+++ src/main/java/com/example/opentravel/test/BlogControllerTest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BlogControllerTest {
 
 }
