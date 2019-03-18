f46d17282ccb30a18bf506b70e910b713ae5dd34
EdilKamchybekov <34943740+edilk@users.noreply.github.com>
2019-03-18 20:18:41 +0600

    SQL
    SQL for admin and blog

---
+++ src/main/resources/sql/admin.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM telegrambot.admin
+LIMIT 0, 1000
+
+-- Date: 2019-02-05 15:36
+*/
+INSERT INTO `admin` (`id`,`active`,`password`,`username`,`fullname`,`phone_number`,`psw`) VALUES (2,1,'$2a$10$ofDlPXgMniiccG4OvMRkT.KMtuNmh6vv7k1khmj7b4ab5zz1Kid5y','123456','hfgsdfs',NULL,1);
+INSERT INTO `admin` (`id`,`active`,`password`,`username`,`fullname`,`phone_number`,`psw`) VALUES (21,1,'$2a$10$LkTH8Ow7vrCGEgGRpf.55Ovf6D4GqNgB2de5T2GteXlZBjLJrkWuy','1234567','Сергей Петров','+77052767048',0);
