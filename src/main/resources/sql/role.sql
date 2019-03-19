49e5608cd7bc217de985d08f85cae5dcd84d60c1
EdilKamchybekov <34943740+edilk@users.noreply.github.com>
2019-03-19 10:37:07 +0600

    SQL

---
+++ src/main/resources/sql/role.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM hciproject.role
+LIMIT 0, 1000
+
+-- Date: 2019-02-06 13:40
+*/
+INSERT INTO `role` (`role_id`,`role`) VALUES (1,'ADMIN');
+INSERT INTO `role` (`role_id`,`role`) VALUES (2,'GID');
+INSERT INTO `role` (`role_id`,`role`) VALUES (3,'TOURIST');
