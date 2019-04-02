cc4e815e2bf4f625d577044052dcabc884c605fb
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2019-04-02 09:54:29 +0600

    file added

--- src/test/java/com/example/opentravel/Sprint1.java
+++ src/test/java/com/example/opentravel/Sprint1.java
@@ -1,15 +1,15 @@
+package com.example.opentravel;
 
 
-/*
 import ch.qos.logback.core.net.SyslogOutputStream;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-/*public class Sprint1 {
-    static String uName="Exa@mail.ru";
-    static String uName2="Example2@mail.ru";
+public class Sprint1 {
+    static String uName="Example123@mail.ru";
+    static String uName2="Examp2@mail.ru";
     static String pWord="zzz123";
     static String exname="Example user";
     static String exage="21";
@@ -19,30 +19,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
     static String excount="KG";
 
 
-    String baseUrl = "localhost:8080";
+    String baseUrl = "http://127.0.0.1:8080/";
 
-    /*public static boolean regGID(WebDriver driver){
+    public static boolean regGID(WebDriver driver,Sprint1 sp){
         String title = "";
         String expectedTitle = "Registration";
 
-        driver.get(this.baseUrl);
-        driver.findElement(By.linkText("REGISTER"));
+        driver.get(sp.baseUrl);
+        driver.findElement(By.linkText("REGISTER")).click();
         title=driver.getTitle();
-        if(title!=expectedTitle) {
+        if(!title.equals( expectedTitle)) {
             System.out.println("Failed:" + title);
             return false;
         }
         driver.findElement(By.linkText("GID")).click();
-        driver.findElement(By.id("name")).sendKeys(this.exname);
-        driver.findElement(By.id("languages")).sendKeys(this.exlang);
-        driver.findElement(By.id("age")).sendKeys(this.exage);
-        driver.findElement(By.id("experience")).sendKeys(this.exexp);
-        driver.findElement(By.id("aboutMe")).sendKeys(this.exabout);
-        driver.findElement(By.id("email")).sendKeys(this.uName);
-        driver.findElement(By.id("password")).sendKeys(this.pWord);
-        driver.findElement(By.linkText(("SIGN UP"))).click();
+        driver.findElement(By.id("name")).sendKeys(sp.exname);
+        driver.findElement(By.id("languages")).sendKeys(sp.exlang);
+        driver.findElement(By.id("age")).sendKeys(sp.exage);
+        driver.findElement(By.id("experience")).sendKeys(sp.exexp);
+        driver.findElement(By.id("aboutMe")).sendKeys(sp.exabout);
+        driver.findElement(By.id("email")).sendKeys(sp.uName);
+        driver.findElement(By.id("password")).sendKeys(sp.pWord);
+        driver.findElement(By.className(("login100-form-btn"))).click();
         title=driver.getTitle();
-        if(title == "Login") {
+        System.out.println(title);
+        if(title.equals( "Login")) {
             System.out.println("GID registration successful!!!");
             return true;
         }
@@ -52,29 +53,29 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 
-    public static boolean regTourist(WebDriver driver){
+    public static boolean regTourist(WebDriver driver,Sprint1 sp){
 
         String title = "";
         String expectedTitle = "Registration";
 
-        driver.get(this.baseUrl);
-        driver.findElement(By.linkText("REGISTER"));
+        driver.get(sp.baseUrl);
+        driver.findElement(By.linkText("REGISTER")).click();
         title=driver.getTitle();
-        if(title!=expectedTitle) {
+        if(!title.equals( expectedTitle)) {
             System.out.println("Failed:" + title);
             return false;
         }
-        driver.findElement(By.linkText("GID")).click();
-        driver.findElement(By.id("name")).sendKeys(this.exname2);
-        driver.findElement(By.id("languages")).sendKeys(this.exlang);
-        driver.findElement(By.id("age")).sendKeys(this.exage);
-        driver.findElement(By.id("country")).sendKeys(this.excount);
-        driver.findElement(By.id("aboutMe")).sendKeys(this.exabout);
-        driver.findElement(By.id("email")).sendKeys(this.uName2);
-        driver.findElement(By.id("password")).sendKeys(this.pWord);
-        driver.findElement(By.linkText(("SIGN UP"))).click();
+        driver.findElement(By.linkText("Tourist")).click();
+        driver.findElement(By.id("name")).sendKeys(sp.exname);
+        driver.findElement(By.id("languages")).sendKeys(sp.exlang);
+        driver.findElement(By.id("age")).sendKeys(sp.exage);
+        driver.findElement(By.id("country")).sendKeys(sp.excount);
+        driver.findElement(By.id("aboutMe")).sendKeys(sp.exabout);
+        driver.findElement(By.id("email")).sendKeys(sp.uName2);
+        driver.findElement(By.id("password")).sendKeys(sp.pWord);
+        driver.findElement(By.className(("login100-form-btn"))).click();
         title=driver.getTitle();
-        if(title == "Login") {
+        if(title.equals( "Login")) {
             System.out.println("Tourist registration successful!!!");
             return true;
         }
@@ -82,89 +83,161 @@ import org.openqa.selenium.firefox.FirefoxDriver;
         return false;
     }
 
-    public static boolean logIn(WebDriver driver) {
+    public static boolean logIn(WebDriver driver, Sprint1 sp) {
         String title = "";
         String expectedTitle = "Login";
 
-        driver.get(this.baseUrl);
-        driver.findElement(By.linkText("LOGIN"));
+        driver.get(sp.baseUrl);
+        driver.findElement(By.linkText("LOGIN")).click();
         title=driver.getTitle();
-        if(title!=expectedTitle) {
+        if(!title.equals( expectedTitle)) {
             System.out.println("Failed:" + title);
             return false;
         }
-        driver.findElement(By.id("email")).sendKeys(this.uName);
-        driver.findElement(By.id("password")).sendKeys(this.pWord);
-        driver.findElement(By.linkText(("SIGN IN"))).click();
+        driver.findElement(By.id("email")).sendKeys(sp.uName);
+        driver.findElement(By.id("password")).sendKeys(sp.pWord);
+        driver.findElement(By.className(("container-login100-form-btn"))).click();
         title=driver.getTitle();
-        if(title == this.baseUrl) {
-            System.out.println("Tourist registration successful!!!");
+        if(title.equals(sp.baseUrl)) {
+            System.out.println("Logged In!!!");
             return true;
         }
         System.out.println("Failed in registration Tourist");
         return false;
     }
 
-    public static boolean USE2(WebDriver driver) {
+    public static boolean USE2(WebDriver driver, Sprint1 sp) {
         String title = "";
-        String expectedTitle = "Login";
+        String expectedTitle = "Profile";
 
-        driver.get(this.baseUrl);
+        driver.get(sp.baseUrl);
         driver.findElement(By.partialLinkText("@gmail.com")).click();
+        title=driver.getTitle();
+        if(!title.equals(expectedTitle)){
+            System.out.println("Failed:" + title);
+            return false;
+        }
+        System.out.println("User's Profile Done!!!");
+        return true;
+    }
 
-    }*/
+    public static boolean BLO4(WebDriver driver, Sprint1 sp) {
+        String title = "";
+        String expectedTitle = "Login";
 
-   /* public static void main(String[] args) {
+        driver.get(sp.baseUrl);
+        driver.findElement(By.partialLinkText("@gmail.com")).click();
+        title=driver.getTitle();
+        if(!title.equals(expectedTitle)){
+            System.out.println("Failed:" + title);
+            return false;
+        }
+
+        driver.findElement(By.linkText("NEW BLOG"));
+        driver.findElement(By.id("title")).sendKeys("hehehe");
+        driver.findElement(By.id("text")).sendKeys("hahaha");
+        driver.findElement(By.id("category")).click();
+        driver.findElement(By.className("btn btn-primary py-3 px-5")).click();
+
+        return true;
+    }
+
+    public static boolean BLO5(WebDriver driver, Sprint1 sp) {
+        String title = "";
+        String expectedTitle = "Login";
+
+        driver.get(sp.baseUrl);
+        driver.findElement(By.partialLinkText("@gmail.com")).click();
+        title=driver.getTitle();
+        if(!title.equals(expectedTitle)){
+            System.out.println("Failed:" + title);
+            return false;
+        }
+
+
+        return true;
+    }
+
+    public static boolean LUP6(WebDriver driver, Sprint1 sp) {
+        String title = "";
+        String expectedTitle = "Login";
+
+        driver.get(sp.baseUrl);
+
+        return true;
+    }
+
+    public static boolean LUP7(WebDriver driver, Sprint1 sp) {
+        String title = "";
+        String expectedTitle = "Login";
+
+        driver.get(sp.baseUrl);
+        return true;
+    }
+
+
+    public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver","C:/Users/tanya/Downloads/programs/geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
         Sprint1 sp=new Sprint1();
         boolean progress;
-        /*progress=regGID(driver);
+//        progress=regGID(driver,sp);
+//        if(!progress){
+//            driver.close();
+//            System.exit(0);
+//            return;
+//        }
+        progress=regTourist(driver,sp);
         if(!progress){
             driver.close();
             System.exit(0);
             return;
         }
-        progress=regTourist(driver);
+        progress=logIn(driver,sp);
         if(!progress){
             driver.close();
             System.exit(0);
             return;
-        }*/
-
-       /* String expectedTitle = "Registration";
-        String st="";
-        st=sp.baseUrl;
-        driver.get("http://127.0.0.1:8080/");
-        driver.findElement(By.linkText("REGISTER")).click();
-        String title=driver.getTitle();
-        if(!title.equals(expectedTitle)) {
-            System.out.println("Failed:" + expectedTitle);
-            return ;
         }
-        driver.findElement(By.linkText("GID")).click();
-        st=sp.exname;
-        driver.findElement(By.id("name")).sendKeys(st);
-        driver.findElement(By.id("languages")).sendKeys(sp.exlang);
-        driver.findElement(By.id("age")).sendKeys(sp.exage);
-        driver.findElement(By.id("experience")).sendKeys(sp.exexp);
-        driver.findElement(By.id("aboutMe")).sendKeys(sp.exabout);
-        driver.findElement(By.id("email")).sendKeys(sp.uName);
-        driver.findElement(By.id("password")).sendKeys(sp.pWord);
-        driver.findElement(By.className(("login100-form-btn"))).click();
-        title=driver.getTitle();
-        if(title.equals("Login")) {
-            System.out.println("GID registration successful!!!");
+        progress=USE2(driver,sp);
+        if(!progress){
+            driver.close();
+            System.exit(0);
+            return;
         }
-        else
-        System.out.println("Failed in registration GID"+title);
 
+        progress=BLO4(driver,sp);
+        if(!progress){
+            driver.close();
+            System.exit(0);
+            return;
+        }
+
+        progress=BLO5(driver,sp);
+        if(!progress){
+            driver.close();
+            System.exit(0);
+            return;
+        }
+
+        progress=LUP6(driver,sp);
+        if(!progress){
+            driver.close();
+            System.exit(0);
+            return;
+        }
+
+
+        progress=LUP7(driver,sp);
+        if(!progress){
+            driver.close();
+            System.exit(0);
+            return;
+        }
 
         driver.close();
         System.exit(0);
 
     }
 }
-}
-        */
