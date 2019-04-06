f14fbe0114f59c9903f67fc4261903bb9c28f999
medabek <mederbek.abdyldaev@iaau.edu.kg>
2019-04-07 02:25:00 +0600

    UPDATE

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -11,10 +11,6 @@ var stompClient = null;
 var username = null;
 var avatar = null;
 
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-];
 
 function connect(event) {
     username = document.querySelector('#name').value.trim();
@@ -85,9 +81,11 @@ function onMessageReceived(payload) {
         messageElement.classList.add('chat-message');
 
         var avatarElement = document.createElement('i');
+        // var avatarText = document.createTextNode(message.sender[0]);
+        // avatarElement.appendChild(avatarText);
+        // avatarElement.style['background-color'] = getAvatarColor(message.sender);
         avatarElement.style['background'] = "url("+avatar+")";
         avatarElement.style['background-size'] = "cover";
-
         messageElement.appendChild(avatarElement);
 
         var usernameElement = document.createElement('span');
@@ -107,15 +105,6 @@ function onMessageReceived(payload) {
 }
 
 
-function getAvatarColor(messageSender) {
-    var hash = 0;
-    for (var i = 0; i < messageSender.length; i++) {
-        hash = 31 * hash + messageSender.charCodeAt(i);
-    }
-
-    var index = Math.abs(hash % colors.length);
-    return colors[index];
-}
 
 usernameForm.addEventListener('submit', connect, true)
 messageForm.addEventListener('submit', sendMessage, true)
