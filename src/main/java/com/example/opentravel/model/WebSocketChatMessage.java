59615db1224536952a5f07b64324575c32b6245e
medabek <mederbek.abdyldaev@iaau.edu.kg>
2019-04-06 13:46:58 +0600

    WebSocketChatMessage model added

---
+++ src/main/java/com/example/opentravel/model/WebSocketChatMessage.java
@@ -0,0 +1,32 @@
+package com.example.opentravel.model;
+
+public class WebSocketChatMessage {
+
+    private String type;
+    private String content;
+    private String sender;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
