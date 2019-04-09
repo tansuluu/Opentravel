65f12b0b1dd636bdf862b6e26896a57ba2bc4616
nadyrbek97 <nadyrbek97@gmail.com>
2019-04-09 10:29:48 +0600

    place comment added

--- src/main/java/com/example/opentravel/service/PlaceCommentService.java
+++ src/main/java/com/example/opentravel/service/PlaceCommentService.java
@@ -1,8 +1,26 @@
 package com.example.opentravel.service;
 
+import com.example.opentravel.model.PlaceComment;
+import com.example.opentravel.model.User;
+import com.example.opentravel.repository.PlaceCommentRepository;
+import com.example.opentravel.repository.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("PlaceCommentService")
 public class PlaceCommentService {
+
+    @Autowired
+    private PlaceCommentRepository placeCommentRepository;
+
+    public List<PlaceComment> findByUser(User user){
+        return placeCommentRepository.findByUser(user);
+    }
+
+    public PlaceComment findById(long id){
+        return placeCommentRepository.findById(id);
+    }
 
 }
