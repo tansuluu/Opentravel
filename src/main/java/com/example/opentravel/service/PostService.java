a451a4a3a9c4c7b0d95ae45d3b1965183df9b52b
nadyrbek97 <nadyrbek97@gmail.com>
2019-04-12 22:42:58 +0600

    Write post to user page

---
+++ src/main/java/com/example/opentravel/service/PostService.java
@@ -0,0 +1,24 @@
+package com.example.opentravel.service;
+
+import com.example.opentravel.model.Post;
+import com.example.opentravel.model.User;
+import com.example.opentravel.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("BlogService")
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findByid(long id){
+        return postRepository.findById(id);
+    }
+
+    public List<Post> findByUser(User user){
+        return postRepository.findByUser(user);
+    }
+}
