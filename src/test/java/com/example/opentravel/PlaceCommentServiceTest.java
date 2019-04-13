37b01c27b5b8a9b541c6976fc01cf7b467718713
Your Name <mederbek.abdyldaev@iaau.edu.kg>
2019-04-13 13:57:05 +0600

    PlaceCommentServiceTest is added

---
+++ src/test/java/com/example/opentravel/PlaceCommentServiceTest.java
@@ -0,0 +1,87 @@
+package com.example.opentravel;
+
+import com.example.opentravel.model.*;
+import com.example.opentravel.repository.CommentBlogRepository;
+import com.example.opentravel.repository.PlaceCommentRepository;
+import com.example.opentravel.service.CommentBlogService;
+import com.example.opentravel.service.PlaceCommentService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+@RunWith(MockitoJUnitRunner.class)
+public class PlaceCommentServiceTest {
+    @Mock
+    PlaceCommentRepository placeCommentRepositoryMock;
+
+    @Autowired
+    PlaceCommentRepository placeCommentRepository;
+
+    @InjectMocks
+    PlaceCommentService placeCommentService;
+
+    @Before
+    public void setUp(){
+        System.out.println("Starting PlaceCommentServiceTest Class");
+    }
+
+    @Test
+    public void testFindByUser(){
+        ArrayList<PlaceComment> list=new ArrayList<>();
+        PlaceComment placeComment=new PlaceComment();
+        User user1=new User();
+        String name = "Meder";
+        user1.setName(name);
+        placeComment.setUser(user1);
+        User user=new User();
+        user.setName("Meder");
+        list.add(placeComment);
+        when(placeCommentRepositoryMock.findByUser(user)).thenReturn(list);
+        assertEquals(list,placeCommentService.findByUser(user));
+    }
+
+    @Test
+    public void testFindById(){
+        PlaceComment placeComment = new PlaceComment();
+        long id = 1;
+        placeComment.setId(id);
+        when(placeCommentRepositoryMock.findById(id)).thenReturn(placeComment);
+        assertEquals(placeComment,placeCommentRepositoryMock.findById(id));
+    }
+
+    @Test
+    public void testFindByIdWhenNoSuchId(){
+        PlaceComment placeComment=new PlaceComment();
+        long id=0;
+        when(placeCommentRepositoryMock.findById(id)).thenReturn(placeComment);
+        assertEquals(placeComment,placeCommentRepositoryMock.findById(id));
+    }
+
+    @Test
+    public void testFindByIdAndPlace(){
+        PlaceComment placeComment = new PlaceComment();
+        Place place = new Place();
+        long id = 1;
+        place.setId(id);
+        when(placeCommentRepositoryMock.findByIdAndPlace(id, place)).thenReturn(placeComment);
+        assertEquals(placeComment,placeCommentRepositoryMock.findByIdAndPlace(id, place));
+    }
+
+    @After
+    public void terminate(){
+        System.out.println("Terminating Test Class");
+    }
+}
