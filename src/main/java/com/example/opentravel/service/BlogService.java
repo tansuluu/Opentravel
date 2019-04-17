8b0970be7b658ff234ebec3991dadc6e4329ac6f
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2019-04-17 23:43:54 +0600

    Model added

--- src/main/java/com/example/opentravel/service/BlogService.java
+++ src/main/java/com/example/opentravel/service/BlogService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.Calendar;
