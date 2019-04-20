07bc771d5821858a5981e04c898c8992ae0a0450
nadyrbek97 <nadyrbek97@gmail.com>
2019-04-20 22:04:25 +0600

    Comment update to blog added

--- src/main/java/com/example/opentravel/controller/BlogController.java
+++ src/main/java/com/example/opentravel/controller/BlogController.java
@@ -146,4 +146,13 @@ public class BlogController {
         return ResponseEntity.ok(commentBlog);
     }
 
+    @RequestMapping(value="/updateBlogComment",method = RequestMethod.GET,produces = "application/json")
+    public ResponseEntity<?> updateBlogComment(@RequestParam("id") long id){
+        System.out.println("gdfsafdgbfdv");
+        CommentBlog commentBlog=commentBlogService.findById(id);
+        System.out.println("gfubidjod");
+        System.out.println(commentBlog);
+        return ResponseEntity.ok(commentBlog);
+    }
+
 }
