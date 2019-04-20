3ec8eafc5cdb7345df70d6edfc3fdfca9e29fa12
nadyrbek97 <nadyrbek97@gmail.com>
2019-04-20 23:48:26 +0600

    Put like to blog

--- src/main/java/com/example/opentravel/repository/LikesRepository.java
+++ src/main/java/com/example/opentravel/repository/LikesRepository.java
@@ -1,9 +1,21 @@
 package com.example.opentravel.repository;
 
+import com.example.opentravel.model.Blog;
 import com.example.opentravel.model.Likes;
+import com.example.opentravel.model.Place;
+import com.example.opentravel.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikesRepository extends JpaRepository<Likes,Long> {
+
+    int removeByUserAndBlog(User user, Blog blog);
+
+    int removeByPlaceAndUser(Place place, User user);
+
+    boolean existsByBlogAndUser(Blog blog, User user);
+
+    boolean existsByPlaceAndUser(Place place, User user);
+
 }
