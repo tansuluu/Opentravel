4ae5b7464aa230e66aeef8256578bd11497524bd
medabek <mederbek.abdyldaev@iaau.edu.kg>
2019-04-21 15:41:12 +0600

    Favorite Repository and Service added

---
+++ src/main/java/com/example/opentravel/controller/FavoriteController.java
@@ -0,0 +1,9 @@
+package com.example.opentravel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+@Controller
+@Transactional
+public class FavoriteController {
+}
