85196f2b29408e2b7fb60e27b4fccc9396c22953
medabek <mederbek.abdyldaev@iaau.edu.kg>
2019-04-22 16:38:32 +0600

    Data table is adding

--- src/main/java/com/example/opentravel/controller/AdminController.java
+++ src/main/java/com/example/opentravel/controller/AdminController.java
@@ -2,10 +2,14 @@ package com.example.opentravel.controller;
 
 import com.example.opentravel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @Transactional
@@ -17,6 +21,8 @@ public class AdminController {
     @RequestMapping("/admin")
     public String admin(Model model){
         model.addAttribute("users",userService.getAll());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
         return "adminUsers";
     }
 }
