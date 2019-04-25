d07e86f31db1f664717f4b0b978afd025bbc0c00
nadyrbek97 <nadyrbek97@gmail.com>
2019-04-25 14:42:52 +0600

    Contact message form working know

--- src/main/java/com/example/opentravel/service/ContactMessageService.java
+++ src/main/java/com/example/opentravel/service/ContactMessageService.java
@@ -2,12 +2,16 @@ package com.example.opentravel.service;
 
 
 import com.example.opentravel.model.ContactMessage;
+import com.example.opentravel.model.User;
 import com.example.opentravel.repository.ContactMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 
 @Service("contactMessageService")
 public class ContactMessageService {
@@ -27,10 +31,24 @@ public class ContactMessageService {
         contactMessage.setDate(Calendar.getInstance().getTime());
         contactMessage.setAnswered(0);
         contactMessageRepository.save(contactMessage);
-
     }
 
     public void delete(long id){
         contactMessageRepository.delete(findById(id));
+    }
+
+    public void sendTokenToReset(User user, HttpServletRequest request){
+        user.setToken(UUID.randomUUID().toString());
+
+        String appUrl = request.getScheme() + "://" + request.getServerName();
+
+        SimpleMailMessage registrationEmail = new SimpleMailMessage();
+        registrationEmail.setTo(user.getEmail());
+        registrationEmail.setSubject("Password reset");
+        registrationEmail.setText("Hello "+ user.getName()+"!\n" +
+                "To reset your password on opentravel site, please click the link below:\n"
+                + appUrl + ":8080/reset?token=" + user.getToken());
+        registrationEmail.setFrom("noreply@domain.com");
+        emailService.sendEmail(registrationEmail);
     }
 }
